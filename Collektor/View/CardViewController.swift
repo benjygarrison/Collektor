776c51b54bdfe71bb671e9bc455cc2976a8a634f
benjyGarrison <benjygarrison@gmail.com>
2019-11-25 13:38:25 -0800

    Deck view controller built

---
+++ Collektor/View/CardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CardViewController.swift
+//  Collektor
+//
+//  Created by Benjamin Garrison on 11/25/19.
+//  Copyright © 2019 Benji Garrison. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CardViewController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
