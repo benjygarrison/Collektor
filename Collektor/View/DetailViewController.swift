c0bddd6b0c9c264c467dbac6804625926a0cf521
benjyGarrison <benjygarrison@gmail.com>
2019-11-28 01:48:08 -0800

    view elements staying in place, background added programmatically

--- Collektor/View/DetailViewController.swift
+++ Collektor/View/DetailViewController.swift
@@ -12,8 +12,24 @@ import CoreData
 class DetailViewController : UIViewController {
     
     
+    let backgroundView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setBackground()
+    }
+    
+    
+    func setBackground() {
+        view.addSubview(backgroundView)
+        //backgroundView.backgroundColor = .lightGray
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        backgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        backgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        view.sendSubviewToBack(backgroundView)
     }
     
     
