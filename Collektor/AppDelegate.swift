bb39939e837b6bb1ac5e53d65e334251e2aa265d
benjyGarrison <benjygarrison@gmail.com>
2019-11-29 14:38:42 -0800

    Static tableview cells in details view

--- Collektor/AppDelegate.swift
+++ Collektor/AppDelegate.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 import CoreData
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        IQKeyboardManager.shared.enable = true
         
         return true
     }
--- Collektor/View/DetailViewController.swift
+++ Collektor/View/DetailViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 import CoreData
 
-class DetailViewController : UIViewController {
+class DetailViewController : UITableViewController {
+    
     
     
     let backgroundView = UIView()
@@ -17,21 +18,10 @@ class DetailViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setBackground()
+        
     }
     
-    
-    func setBackground() {
-        view.addSubview(backgroundView)
-        //backgroundView.backgroundColor = .lightGray
-        backgroundView.translatesAutoresizingMaskIntoConstraints = false
-        backgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        backgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        backgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        view.sendSubviewToBack(backgroundView)
-    }
-    
+
     
 }
 
