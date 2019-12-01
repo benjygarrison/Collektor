34c3415f577995dbe172b8bd07db9f38890a2d0a
benjyGarrison <benjygarrison@gmail.com>
2019-12-01 14:35:15 -0800

    sliders and slider labels working properly

--- Collektor/View/DetailViewController.swift
+++ Collektor/View/DetailViewController.swift
@@ -13,6 +13,13 @@ class DetailViewController : UITableViewController {
     
     @IBOutlet weak var cardName: UILabel!
     
+    @IBOutlet weak var conSlider: UISlider!
+    @IBOutlet weak var conLabel: UILabel!
+    
+    @IBOutlet weak var scoreSlider: UISlider!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
     var detailArray = [Details]()
     var selectedCard: Card? {
         
@@ -53,6 +60,58 @@ class DetailViewController : UITableViewController {
         }
     
 
+
+//MARK: - slider functions
+
+    
+    @IBAction func conditionSlider(_ sender: UISlider) {
+        
+        let step: Float = 1
+        let roundedStep = round(conSlider.value / step) * step
+        conSlider.value = roundedStep
+        
+        
+            switch conSlider.value {
+            case -5: conLabel.text = "Poor"
+            case -4: conLabel.text = "Fair"
+            case -3: conLabel.text = "Good"
+            case -2: conLabel.text = "Very Good"
+            case -1: conLabel.text = "Very Good-Excellent"
+            case 0: conLabel.text = "Excellent"
+            case 1: conLabel.text = "Excellent-Mint"
+            case 2: conLabel.text = "Near Mint"
+            case 3: conLabel.text = "Near Mint-Mint"
+            case 4: conLabel.text = "Mint"
+            case 5: conLabel.text = "Gem Mint"
+            default:
+                conSlider.value = 0
+            }
+        
+        
+        
+    }
+    
+    @IBAction func scoreSlider(_ sender: UISlider) {
+        
+        let step: Float = 1
+        let roundedStep = round(scoreSlider.value / step) * step
+        scoreSlider.value = roundedStep
+        
+        switch scoreSlider.value {
+        case -4: scoreLabel.text = "1"
+        case -3: scoreLabel.text = "2"
+        case -2: scoreLabel.text = "3"
+        case -1: scoreLabel.text = "4"
+        case 0: scoreLabel.text = "5"
+        case 1: scoreLabel.text = "6"
+        case 2: scoreLabel.text = "7"
+        case 3: scoreLabel.text = "8"
+        case 4: scoreLabel.text = "9"
+        case 5: scoreLabel.text = "10"
+        default:
+            scoreSlider.value = 0
+        }
+    }
     
 }
 
