1ae0141911ebabb3c73c6d7d81f6d01727a8dc98
benjyGarrison <benjygarrison@gmail.com>
2019-12-24 23:12:01 -0800

    Able to pull series and decks from firestore. Delete firestorm and build local variables in future commits

--- Collektor/Controller/SeriesDownloadViewController.swift
+++ Collektor/Controller/SeriesDownloadViewController.swift
@@ -17,7 +17,8 @@ class SeriesDownloadViewController: UITableViewController {
     
     let database = Firestore.firestore()
     var seriesArray = [Series]()
-
+    var deckArray = [Deck]()
+    var cardArray = [Card]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +26,7 @@ class SeriesDownloadViewController: UITableViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
-        loadData()
+        loadSeriesData()
     }
     
     
@@ -64,14 +65,65 @@ class SeriesDownloadViewController: UITableViewController {
          
             let newSeries = Series()
             newSeries.seriesName = clickedSeries
-            
-             do {
-                    try self.realm.write {
-                    self.realm.add(newSeries)
-                    }
-                } catch {
-                    print("Error downloading series \(error)")
+             
+            do {
+               try self.realm.write {
+               self.realm.add(newSeries)
+                   }
+               } catch {
+                   print("Error downloading series \(error)")
                 }
+            
+            let deckQuery = self.database.collection("decks").whereField("parentSeries", isEqualTo: clickedSeries)
+            
+            deckQuery.getDocuments { (snapshot, error) in
+                if error != nil {
+                    print(error as Any)
+                } else {
+                    for document in (snapshot?.documents)! {
+                        if let downloadedDeckName = document.data()["deckName"] as? String {
+                            print(downloadedDeckName)
+                            
+                            let newDeck = Deck()
+                            newDeck.deckName = downloadedDeckName
+                            self.deckArray.append(newDeck)
+                            let parentSeries = newSeries
+
+                            do {
+                                try self.realm.write {
+                                parentSeries.decks.append(newDeck)
+                                    }
+                                } catch {
+                                print("Error downloading series \(error)")
+                            }
+                            
+//                            self.database.collection("cards").whereField("parentDeck", isEqualTo: downloadedDeckName).getDocuments { (snapshot, error) in
+//                                if error != nil {
+//                                    print(error as Any)
+//                                } else {
+//                                    if let downloadedCardName = document.data()["cardName"] as? String, let downloadedCardNumber = document.data()["cardNumber"] as? String {
+//                                    print(downloadedCardName)
+//
+//                                    let newCard = Card()
+//                                    newCard.cardName = downloadedCardName
+//                                    newCard.cardNumber = downloadedCardNumber
+//                                    self.cardArray.append(newCard)
+//                                    let parentDeck = newDeck
+//
+//                                    do {
+//                                        try self.realm.write {
+//                                        parentDeck.cards.append(newCard)
+//                                            }
+//                                        } catch {
+//                                        print("Error downloading series \(error)")
+//                                        }
+//                                    }
+//                                }
+//                            }
+                        }
+                    }
+                }
+            }
         }
 
         alert.addAction(action)
@@ -82,7 +134,7 @@ class SeriesDownloadViewController: UITableViewController {
     
     //MARK: - load series
     
-    func loadData() {
+    func loadSeriesData() {
         database.collection("series").getDocuments { (snapshot, error) in
             if error != nil {
                 print(error as Any)
@@ -101,6 +153,8 @@ class SeriesDownloadViewController: UITableViewController {
             }
         }
     }
+    
+    
     
     
 }
